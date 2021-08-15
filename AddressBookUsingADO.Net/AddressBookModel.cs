fb0d9c8de4316b60cf85d089085812e297b74f04
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-16 00:53:59 +0530

    [Ravina] Added : Defined all the properties

---
+++ AddressBookUsingADO.Net/AddressBookModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookUsingADO.Net
+{
+
+	public class AddressBookModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string ZipCode { get; set; }
+		public string PhoneNumber { get; set; }
+		public string EmailId { get; set; }
+	}
+}
+
