f6cb8a9ea9346f394b1557bb1c4d5279c8bd6cf9
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-16 00:53:38 +0530

    [Ravina] Added : Added methods to add update and delete the following properties

---
+++ AddressBookUsingADO.Net/AddressBook.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookUsingADO.Net
+{
+        public class AddressBook
+        {
+
+            SqlConnection Connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBook_service;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");  
+            SqlCommand command;
+
+            public bool AddContacts(AddressBookModel model)
+            {
+                try
+                {
+                    using (Connection)
+                    {
+                        command = new SqlCommand("SpAddContacts", Connection);
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Action", "Insert");
+                        command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                        command.Parameters.AddWithValue("@LastName", model.LastName);
+                        command.Parameters.AddWithValue("@Address", model.Address);
+                        command.Parameters.AddWithValue("@City", model.City);
+                        command.Parameters.AddWithValue("@State", model.State);
+                        command.Parameters.AddWithValue("@ZipCode", model.ZipCode);
+                        command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                        command.Parameters.AddWithValue("@EmailId", model.EmailId);
+
+                        Connection.Open();
+                        var result = command.ExecuteNonQuery();
+                        Connection.Close();
+
+                        if (result != 0)
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Connection.Close();
+                }
+                return false;
+            }
+
+            public bool EditContact(AddressBookModel model)
+            {
+                try
+                {
+                    using (Connection)
+                    {
+                        command = new SqlCommand("SpAddContacts", Connection);
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@Action", "Update");
+                        command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                        command.Parameters.AddWithValue("@LastName", model.LastName);
+                        command.Parameters.AddWithValue("@Address", model.Address);
+                        command.Parameters.AddWithValue("@City", model.City);
+                        command.Parameters.AddWithValue("@State", model.State);
+                        command.Parameters.AddWithValue("@ZipCode", model.ZipCode);
+                        command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                        command.Parameters.AddWithValue("@EmailId", model.EmailId);
+
+                        Connection.Open();
+                        var result = command.ExecuteNonQuery();
+                        Connection.Close();
+
+                        if (result != 0)
+                        {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Connection.Close();
+                }
+                return false;
+            }
+
+            public bool DeleteContact(AddressBookModel model)
+            {
+                try
+                {
+                    command = new SqlCommand("SpAddContacts", Connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Action", "Delete");
+                    command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                    command.Parameters.AddWithValue("@LastName", model.LastName);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@City", model.City);
+                    command.Parameters.AddWithValue("@State", model.State);
+                    command.Parameters.AddWithValue("@ZipCode", model.ZipCode);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@EmailId", model.EmailId);
+
+                    Connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    Connection.Close();
+
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Connection.Close();
+                }
+                return false;
+            }
+        }
+}
+
